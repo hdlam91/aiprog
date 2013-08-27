5104f653e7bd52426d75c748c6c2ac03388151f7
hdlam <instagibno@gmail.com>
2013-08-27 15:26:55 +0200

    psuh so i can see what you did

--- minMaxQuarto/src/minMaxQuarto/Quarto.java
+++ minMaxQuarto/src/minMaxQuarto/Quarto.java
@@ -13,7 +13,7 @@ public class Quarto {
 			case 2:
 				return "novice";
 			case 3:
-				return "minmax";
+				return "minimax";
 			default:
 				return "";
 		}
@@ -23,39 +23,41 @@ public class Quarto {
 	public static void main(String[] args) {
 		{
 			//0 = human, 1 = random, 2 = novice, 3 = minmax
-		      int player1 = 0;
+		      int player1 = -1;
 		      int player1depth = 2;
-		      int player2 = 0;
+		      int player2 = -1;
 		      int player2depth = 2;
 		      int b,c;
 		 
 		      Scanner in = new Scanner(System.in);
 		      
 		      //chooses mode for p1
-		      while(player1 < 4 && player1 >= 0){
-		    	  System.out.println("choose mode for player 1:\n0 = human, 1 = random, 2 = novice, 3 = minmax");
+		      while(!(player1 < 4 && player1 >= 0)){
+		    	  System.out.println("choose mode for player 1:\n0 = human, 1 = random, 2 = novice, 3 = minimax");
 		    	  player1 = in.nextInt();
 		      }
 		      System.out.println("You chose "+ mode(player1) + " for player1");
 		      
 		      //chose depth
 		      if(player1 == 3){
-		    	  System.out.println("choose depth for min max");
+		    	  System.out.println("choose depth for minimax");
 		    	  player1depth = in.nextInt();
 		    	  
 		      }
+		      System.out.println("\n-----------------------------");
 		      
 		      
-		      while(player2 < 4 && player2 >= 0){
-		    	  System.out.println("choose mode for player 2:\n0 = human, 1 = random, 2 = novice, 3 = minmax");
-		      	player2 = in.nextInt();
+		      while(!(player2 < 4 && player2 >= 0)){
+		    	  System.out.println("choose mode for player 2:\n0 = human, 1 = random, 2 = novice, 3 = minimax");
+		    	  player2 = in.nextInt();
 		      }
-		      System.out.println("You chose "+ mode(player2) + " for player2d");
+		      System.out.println("You chose "+ mode(player2) + " for player2");
 		      if(player2 == 3){
-		    	  System.out.println("choose depth for min max");
+		    	  System.out.println("choose depth for minimax");
 		    	  player2depth = in.nextInt();
 		    	  
 		      }
+		      System.out.println("\n-----------------------------");
 		 
 		         
 		   }
