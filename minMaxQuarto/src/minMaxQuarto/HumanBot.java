32b57e507d971ceb9301fa9630f07ca17599617b
hdlam <instagibno@gmail.com>
2013-09-03 15:55:19 +0200

    human bot finished hopefully

--- minMaxQuarto/src/minMaxQuarto/HumanBot.java
+++ minMaxQuarto/src/minMaxQuarto/HumanBot.java
@@ -1,11 +1,16 @@
 package minMaxQuarto;
 
+import java.util.Scanner;
+
 public class HumanBot extends Bot{
 
+	Scanner in;
+	
+	
 	public HumanBot(Board board) {
 		super(board);
 		this.name = "human";
-		// TODO Auto-generated constructor stub
+		in = new Scanner(System.in);
 	}
 	
 	
@@ -16,21 +21,31 @@ public class HumanBot extends Bot{
 	
 	@Override
 	public void placePiece(int x, int y, int index) {
-		// TODO Auto-generated method stub
-		
+		while(!b.placePieceOnBoard(x, y, b.getPieceFromRemaining(index),index)){
+			System.out.println("choose an X, and Y");
+			int X = in.nextInt();
+			int Y = in.nextInt();
+		}
 	}
 
 	@Override
 	public int choosePiece() {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println(" choose piece:");
+		int piece = in.nextInt();
+		while(b.getPieceFromRemaining(piece) == null){
+			System.out.println("\nnot a legal piece, choose again:\n");
+			piece = in.nextInt();
+		}
+		return piece;
 	}
 
 
 	@Override
 	public void choseWheretoPlacePiece(int index) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("choose an X, and Y");
+		int X = in.nextInt();
+		int Y = in.nextInt();
+		placePiece(X, Y, index);
 	}
 
 
