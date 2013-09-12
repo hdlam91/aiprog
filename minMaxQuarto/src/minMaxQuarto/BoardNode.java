b0d406041e44b71d4fe1e7332023045c91cc526e
hdlam <instagibno@gmail.com>
2013-09-12 15:27:40 +0200

    tree building should be complete

--- minMaxQuarto/src/minMaxQuarto/BoardNode.java
+++ minMaxQuarto/src/minMaxQuarto/BoardNode.java
@@ -10,21 +10,26 @@ public class BoardNode {
 	int maxDepth;
 	boolean ourTurn;
 	int placedX, placedY;
+	int chosenPieceIndex;
 	int heuristic;
 	
-	public BoardNode(Board board, int maxDepth) {
+	public BoardNode(Board board, int maxDepth, int pieceIndex) {
 		currentState = new Board(board);
 		this.maxDepth = maxDepth;
-		addChildren();
+		this.depth = 0;
+		chosenPieceIndex = pieceIndex;
+		addChildren(true);
 	}
 	public BoardNode(Board board, int depth, int maxDepth, int x, int y, int index) {
 		currentState = new Board(board);
 		this.maxDepth = maxDepth;
+		this.depth = depth;
 		currentState.placePieceOnBoard(x, y, currentState.getPieceFromRemaining(index), index);
+		chosenPieceIndex = index;
 		placedX = x;
 		placedY = y;
 		if(maxDepth > depth){
-			addChildren();
+			addChildren(false);
 		}
 	}
 	
@@ -35,22 +40,39 @@ public class BoardNode {
 		}
 	}
 	
-	public void addChildren(){
+	public void addChildren(boolean inputPiece){
 		//this method should add all the children that can be added.
-		piece:
-		for (int i = 0; i < 16; i++) {
+		if(inputPiece){
 			for (int y = 0; y < 4; y++) {
 				for (int x = 0; x < 4; x++) {
-					if(currentState.getPieceFromRemaining(i) != null){
-						if(currentState.placePieceOnBoard(x, y, currentState.getPieceFromRemaining(i), i)){
-							BoardNode child = new BoardNode(currentState, depth+1,maxDepth, x,y,i);
+					if(currentState.getPieceFromRemaining(chosenPieceIndex) != null){
+						if(currentState.placePieceOnBoard(x, y, currentState.getPieceFromRemaining(chosenPieceIndex), chosenPieceIndex)){
+							BoardNode child = new BoardNode(currentState, depth+1,maxDepth, x,y,chosenPieceIndex);
 							children.add(child);
 						}
 						else 
 							break;
 					}
-					else
-						break piece;
+				}
+			}
+			
+		}
+		else{
+			piece:
+			for (int i = 0; i < 16; i++) {
+				for (int y = 0; y < 4; y++) {
+					for (int x = 0; x < 4; x++) {
+						if(currentState.getPieceFromRemaining(i) != null){
+							if(currentState.placePieceOnBoard(x, y, currentState.getPieceFromRemaining(i), i)){
+								BoardNode child = new BoardNode(currentState, depth+1,maxDepth, x,y,i);
+								children.add(child);
+							}
+							else 
+								break;
+						}
+						else
+							break piece;
+					}
 				}
 			}
 		}
@@ -71,7 +93,47 @@ public class BoardNode {
 		}
 		//if(depth%2 == 0)
 		heuristic = 0;
-}
+	}
+	
+	public int getHeuristic(){
+		return heuristic;
+	}
+	
+	public void miniMaxDecision(BoardNode state){
+		
+	}
+	
+	public int maxValue(BoardNode state, int alpha, int beta){
+		if(currentState.checkWin())
+			return heuristic;
+		
+		int value = Integer.MIN_VALUE;
+		for (BoardNode c : children) {
+			value = Math.max(value, minValue(state,alpha,beta));
+//			if(c.getHeuristic() > value)
+//				value = getHeuristic();
+			if(value >= beta)
+				return value;
+			alpha = Math.max(alpha,value);
+		}
+		return value;
+	}
+	public int minValue(BoardNode state, int alpha, int beta){
+		if(currentState.checkWin())
+			return heuristic;
+		
+		int value = Integer.MAX_VALUE;
+		for (BoardNode c : children) {
+			value = Math.min(value, maxValue(state,alpha,beta));
+//			if(c.getHeuristic() < value)
+//				value = getHeuristic();
+			if(value >= alpha)
+				return value;
+			beta = Math.min(beta,value);
+		}
+		return value;
+	}
+	
 	
 	public int evaluatedValue(){
 		return heuristic;
