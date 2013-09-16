dd66e230aa287ec875fb09aef6f618ac9305e8bf
Eivind H <evdh15@gmail.com>
2013-09-16 14:29:36 +0200

    Removed unused code and library dependencies

--- minMaxQuarto/src/minMaxQuarto/MinimaxBot.java
+++ minMaxQuarto/src/minMaxQuarto/MinimaxBot.java
@@ -1,15 +1,11 @@
 package minMaxQuarto;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class MinimaxBot extends NoviceBot{
 	protected int depth;
 	private int nextPiecetoGive;
-	BoardNode miniMaxTree;
+//	BoardNode miniMaxTree;
 	Board internalBoard;
-	int numberOfMoves = 0;
+	int numberOfMoves = 2;
 	AlphaBeta minimax;
 	
 	public MinimaxBot(Board board, int depth) {
@@ -17,9 +13,8 @@ public class MinimaxBot extends NoviceBot{
 		this.depth = depth;
 		this.name = "minimax_";
 		minimax = null;
-		miniMaxTree = null;
+//		miniMaxTree = null;
 		internalBoard = new Board(board);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -47,100 +42,12 @@ public class MinimaxBot extends NoviceBot{
 		}
 		else
 			return nextPiecetoGive;
-		
-		/*if(miniMaxTree == null || b.piecePlayed() < numberOfMoves){
-			boolean winState = false;
-			internalBoard = new Board(b);
-			List<Integer> dataList = new ArrayList<Integer>();
-		    for (int i = 0; i < 16; i++) {
-		      dataList.add(i);
-		    }
-		    Collections.shuffle(dataList);
-		    int[] numList = new int[dataList.size()];
-		    for (int i = 0; i < dataList.size(); i++) {
-		      numList[i] = dataList.get(i);
-		    }
-			int numListIndex = 0;
-			
-			
-			Piece pieceOnHand;
-			int index;
-			
-			while(numListIndex < 16){
-				board:
-				for (int i = 0; i < 4; i++) {
-					winState = false;
-					for (int j = 0; j < 4; j++) {
-						index = numList[numListIndex];
-						pieceOnHand = internalBoard.getPieceFromRemaining(index);
-						if(pieceOnHand==null){
-							numListIndex++;
-							winState=true;
-							break board;
-						}
-						
-						if(internalBoard.placePieceOnBoard(i, j, pieceOnHand, index)){
-							if(internalBoard.checkWin()){
-								internalBoard.removePieceOnBoard(i, j);
-								internalBoard.addPieceToRemaining(pieceOnHand, index);
-								numListIndex++;
-								winState = true;
-								break board;
-							}
-							else{
-								internalBoard.removePieceOnBoard(i, j);
-								internalBoard.addPieceToRemaining(pieceOnHand, index);
-							}
-							
-						}
-						
-						
-					}
-				}
-				if(!winState)
-					return numList[numListIndex];
-			}
-			
-			
-			
-			int random = (int)(Math.random()*16);
-			while(b.getPieceFromRemaining(random) == null){
-				random = (int)(Math.random()*16);
-			}
-			return random;
-		}
-		//int pieceIndex = 
-		//return miniMaxTree.pickNextBest().getChosenPieceIndex();
-		*/
 	}
 
 	@Override
 	public void choseWheretoPlacePiece(int index) {
 		if(b.piecePlayed()<numberOfMoves){
 			super.choseWheretoPlacePiece(index);
-//			internalBoard = new Board(b);
-//			Piece pieceOnHand = internalBoard.getPieceFromRemaining(index);
-//			
-//			for (int i = 0; i < 4; i++) {
-//				for (int j = 0; j < 4; j++) {
-//					
-//					if(internalBoard.placePieceOnBoard(i, j, pieceOnHand, index)){
-//						if(internalBoard.checkWin()){
-//							placePiece(i, j, index);
-//							return;
-//						}
-//						else{
-//							internalBoard.removePieceOnBoard(i, j);
-//							internalBoard.addPieceToRemaining(pieceOnHand, index);
-//						}
-//						
-//					}
-//				}
-//			}
-//			int randomX = (int)(Math.random()*4);
-//			int randomY = (int)(Math.random()*4);
-//			placePiece(randomX, randomY, index);
-//			return;
 		}
 		else{
 			minimax = new AlphaBeta(/*index,*/depth,b);
@@ -151,7 +58,6 @@ public class MinimaxBot extends NoviceBot{
 //			miniMaxTree = new BoardNode(b, depth, index);
 //			BoardNode next = miniMaxTree.pickBestNode();
 //			placePiece(next.getPlacedX(), next.getPlacedY(), index);
-		// TODO Auto-generated method stub
 		}
 	}
 
