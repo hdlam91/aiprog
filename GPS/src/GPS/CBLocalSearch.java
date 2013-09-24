6c8bc4bd27e692c3e5bcbcf1e86021435ec23977
Eivind H <evdh15@gmail.com>
2013-09-24 15:16:44 +0200

    File structure

---
+++ GPS/src/GPS/CBLocalSearch.java
@@ -0,0 +1,9 @@
+package GPS;
+
+public class CBLocalSearch {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("stuff");
+	}
+}
--- GPS/src/GPS/Test.java
+++
@@ -1,7 +0,0 @@
-package GPS;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("woop");
-	}
-}
