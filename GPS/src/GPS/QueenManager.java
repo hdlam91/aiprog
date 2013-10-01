015b3463ddcf3701889404e7908f5241b831fad3
Eivind H <evdh15@gmail.com>
2013-10-01 13:27:41 +0200

    stuff anf things

--- GPS/src/GPS/QueenManager.java
+++ GPS/src/GPS/QueenManager.java
@@ -3,17 +3,28 @@ package GPS;
 import java.util.ArrayList;
 
 public class QueenManager extends StateManager{
-	public QueenManager(){
+	private int k;
+	private int[] board; 
+	
+	public QueenManager(int k){
 		super();
+		this.k = k;
+		this.board = new int[k];
 	}
 	
 	public ArrayList<GeneralNode> createChildren(GeneralNode currentGeneralNode2) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	public Object findBestNeighbor(){
+		return null;
+	}
+	
 	public Object createInitState() {
-		// TODO Auto-generated method stub
-		return null;		
+		for (int i = 0; i < k; i++) {
+			board[i] = (int)(Math.random()*k);
+		}
+		return board;
 	}
 }
