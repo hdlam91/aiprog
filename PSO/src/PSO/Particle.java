ed79b4d6faadf971b893f77bfc1a1d138566d986
Eivind H <evdh15@gmail.com>
2013-11-05 13:15:41 +0100

    fixes and stuff

---
+++ PSO/src/PSO/Particle.java
@@ -0,0 +1,74 @@
+package PSO;
+
+public class Particle {
+	private double[] x;
+	private double[] v;
+	private double[] p;
+	private double[] g;
+	static double[] goal; 
+	double c_1, c_2;
+	private int dimensions;
+	
+	public Particle(int dimensions, double[] goal, double c1, double c2) {
+		this.x = new double[dimensions];
+		
+		for (int i = 0; i < dimensions; i++) {
+			x[i] = Math.pow(Math.random()*5,2);
+		}
+		
+		this.p = new double[dimensions];
+		this.v = new double[dimensions];
+		
+		
+		this.setDimensions(dimensions);
+		this.c_1 = c1;
+		this.c_2 = c2;		
+		this.goal = goal;
+	}
+	
+	
+	public void nextIteration(){
+		double r1 = Math.random(), r2 = Math.random();
+		for (int i = 0; i < v.length; i++) {
+			v[i] = v[i]+ (c_1 * r1 *(p[i]-x[i]))+(c_2* r2 *(g[i]-x[i]));
+			x[i] = x[i] + v[i];
+		}
+	}
+	
+	public int getVectorSize(){
+		return x.length;
+	}
+	
+	public double getPosition(int i){
+		if(i>x.length || i < 0){
+			return -1;
+		}
+		else return x[i];
+	}
+	
+	public double[] getPositionVector(){
+		return x;
+	}
+	
+	public double[] getVelocityVector(){
+		return v;
+	}
+	
+	public void updateGlobalPosition(double[] global){
+		g = global;
+	}
+	
+	public void updateLocalPosition(double[] local){
+		this.p = local;
+	}
+
+
+	public int getDimensions() {
+		return dimensions;
+	}
+
+
+	public void setDimensions(int dimensions) {
+		this.dimensions = dimensions;
+	}
+}
