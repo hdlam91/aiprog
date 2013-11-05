0661a32742bd49d291bbdaf2b6108ac7fc881e93
Eivind H <evdh15@gmail.com>
2013-11-05 13:59:09 +0100

    stuff and thigns HD

--- PSO/src/PSO/Circle.java
+++ PSO/src/PSO/Circle.java
@@ -6,4 +6,14 @@ public class Circle extends PSO_problem{
 	public Circle(int dimensions){
 		super(dimensions, numberOfParticles);
 	}
+	
+	public double f(){
+		double f = 0;
+		for (int i = 0; i < numberOfParticles; i++) {
+			for (int j = 0; j < dimensions; j++) {
+				f += Math.pow(particles[i].getPositionVector()[j],2);
+			}
+		}
+		return f;
+	}
 }
