69ac50df9b090cf3364865d044851dd66346ec3e
Eivind H <evdh15@gmail.com>
2013-11-05 13:50:49 +0100

    added circle and stuff

---
+++ PSO/src/PSO/Circle.java
@@ -0,0 +1,9 @@
+package PSO;
+
+public class Circle extends PSO_problem{
+	private static int numberOfParticles = 20;
+	
+	public Circle(int dimensions){
+		super(dimensions, numberOfParticles);
+	}
+}
